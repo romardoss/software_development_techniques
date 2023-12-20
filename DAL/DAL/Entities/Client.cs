cf6944df5e2091c8724068291cd0c89def19d870
romardoss <romassuper@gmail.com>
2023-12-21 00:24:14 +0200

    Added Entities to DAL

---
+++ DAL/DAL/Entities/Client.cs
@@ -0,0 +1,9 @@
+﻿namespace DAL.Entities
+{
+    public class Client
+    {
+        public int ClientID { get; set; }
+        public string Department { get; set; }
+        public IEnumerable<Order> Orders { get; set; }
+    }
+}
---
+++ DAL/DAL/Entities/Order.cs
@@ -0,0 +1,8 @@
+﻿namespace DAL.Entities
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string StaffToDeliver { get; set; }
+    }
+}
