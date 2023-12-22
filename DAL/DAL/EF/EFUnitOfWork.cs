661f2e82dd7e27dec4eee3199d0ca6831b2c9731
romardoss <romassuper@gmail.com>
2023-12-22 21:54:31 +0200

    Added UnitOfWork

---
+++ DAL/DAL/EF/EFUnitOfWork.cs
@@ -0,0 +1,69 @@
+﻿using DAL.Entities;
+using DAL.Repositories.Impl;
+using DAL.Repositories.Interfaces;
+using DAL.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.EF
+{
+    public class EFUnitOfWork : IUnitOfWork
+    {
+        private DeliveryContext db;
+        private ClientRepository clientRepository;
+        private OrderRepository orderRepository;
+
+        public EFUnitOfWork(DbContextOptions options)
+        {
+            db = new DeliveryContext(options);
+        }
+
+        public IClientRepository ClientRepository
+        {
+            get
+            {
+                if (clientRepository == null)
+                {
+                    clientRepository = new ClientRepository(db);
+                }
+                return clientRepository;
+            }
+        }
+
+        public IOrderRepository OrderRepository
+        {
+            get
+            {
+                if(orderRepository == null)
+                {
+                    orderRepository = new OrderRepository(db);
+                }
+                return orderRepository;
+            }
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if(!disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+                disposed = true;
+            }
+        }
+    }
+}
---
+++ DAL/DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using DAL.Repositories.Interfaces;
+
+namespace DAL.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IClientRepository ClientRepository { get; }
+        IOrderRepository OrderRepository { get; }
+        void Save();
+    }
+}
