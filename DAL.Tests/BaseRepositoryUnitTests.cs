4f2272692a0a48ad3ac2441d89b28e92c780114f
romardoss <romassuper@gmail.com>
2024-01-08 22:10:38 +0200

    Added BLL project

--- DAL.Tests/BaseRepositoryUnitTests.cs
+++ DAL.Tests/BaseRepositoryUnitTests.cs
@@ -71,5 +71,59 @@
             mockDbSet.Verify(dbSet => dbSet.Find(expecterOrder.OrderId), Times.Once());
             mockDbSet.Verify(dbSet => dbSet.Remove(expecterOrder), Times.Once());
         }
+
+        /*
+        [Fact]
+        public void GetAll_WithoutParameters_CalledToListMethodOfDbSet()
+        {
+            //Arrange
+            DbContextOptions options = new DbContextOptionsBuilder<DeliveryContext>().Options;
+            var mockContext = new Mock<DeliveryContext>(options);
+            var mockDbSet = new Mock<DbSet<Order>>();
+
+            List<Order> expectedList = new List<Order>(){
+                new() { OrderId = 1 },
+                new() { OrderId = 2 },
+                new() { OrderId = 3 },
+                new() { OrderId = 4 },
+                new() { OrderId = 5 },
+            };
+
+            mockDbSet = new Mock<DbSet<Order>>(GetQueryableMockDbSet<Order>(expectedList));
+
+            mockContext.Setup(context => context.Set<Order>())
+                .Returns(mockDbSet.Object);
+
+            
+
+            
+
+            /*mockDbSet.Setup(mock => mock.ToList())
+                .Returns(expectedList);
+
+            var repository = new TestOrderRepository(mockContext.Object);
+
+
+            //Act
+            IEnumerable<Order> actualOrderList = repository.GetAll();
+
+            //Assert
+            //mockDbSet.Verify(dbSet => dbSet.ToList(), Times.Once());
+            Assert.Equal(expectedList.Count, actualOrderList.Count());
+        }
+
+        private static DbSet<T> GetQueryableMockDbSet<T>(List<T> sourceList) where T : class
+        {
+            var queryable = sourceList.AsQueryable();
+
+            var dbSet = new Mock<DbSet<T>>();
+            dbSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(queryable.Provider);
+            dbSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(queryable.Expression);
+            dbSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
+            dbSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(() => queryable.GetEnumerator());
+            dbSet.Setup(d => d.Add(It.IsAny<T>())).Callback<T>((s) => sourceList.Add(s));
+
+            return dbSet.Object;
+        }*/
     }
 }
